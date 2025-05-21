08b377478b0665a43c005959db27bd8ba7a75bc0
Richard Annand <richard@ohheyrj.co.uk>
2025-05-21 11:43:47 +0100

    fix: reverted default.nix

--- default.nix
+++ default.nix
@@ -1,19 +1,19 @@
-{ pkgs ? import <nixpkgs> {} }:
+# This file describes your repository contents.
+# It should return a set of nix derivations
+# and optionally the special attributes `lib`, `modules` and `overlays`.
+# It should NOT import <nixpkgs>. Instead, you should take pkgs as an argument.
+# Having pkgs default to <nixpkgs> is fine though, and it lets you use short
+# commands such as:
+#     nix-build -A mypackage
+
+{ pkgs ? import <nixpkgs> { } }:
 
 {
-  __functor = self: pkgs:
-    let
-      # Helper to reduce repetition
-      callPackage = pkgs.callPackage;
-    in
-    {
-      # The `lib`, `modules`, and `overlays` names are special
-      lib = import ./lib { inherit pkgs; }; # functions
-      modules = import ./modules;           # NixOS modules
-      overlays = import ./overlays;         # nixpkgs overlays
+  # The `lib`, `modules`, and `overlays` names are special
+  lib = import ./lib { inherit pkgs; }; # functions
+  modules = import ./modules; # NixOS modules
+  overlays = import ./overlays; # nixpkgs overlays
 
-      chatterino = callPackage ./pkgs/chat/chatterino {};
-      kobo-desktop = callPackage ./pkgs/media/kobo-desktop {};
-    };
+  chatterino = pkgs.callPackage ./pkgs/chat/chatterino {};
+  kobo-desktop = pkgs.callPackage ./pkgs/media/kobo-desktop {};
 }
-
