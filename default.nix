e5b835db5ea4aedb883e255895290623e5a437a9
Richard Annand <richard@ohheyrj.co.uk>
2025-05-21 02:14:59 +0100

    chore: fixing packages

--- default.nix
+++ default.nix
@@ -1,20 +1,18 @@
-# This file describes your repository contents.
-# It should return a set of nix derivations
-# and optionally the special attributes `lib`, `modules` and `overlays`.
-# It should NOT import <nixpkgs>. Instead, you should take pkgs as an argument.
-# Having pkgs default to <nixpkgs> is fine though, and it lets you use short
-# commands such as:
-#     nix-build -A mypackage
-
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import <nixpkgs> {} }:
 
 {
-  # The `lib`, `modules`, and `overlays` names are special
-  lib = import ./lib { inherit pkgs; }; # functions
-  modules = import ./modules; # NixOS modules
-  overlays = import ./overlays; # nixpkgs overlays
+  __functor = self: pkgs:
+    let
+      # Helper to reduce repetition
+      callPackage = pkgs.callPackage;
+    in
+    {
+      # The `lib`, `modules`, and `overlays` names are special
+      lib = import ./lib { inherit pkgs; }; # functions
+      modules = import ./modules;           # NixOS modules
+      overlays = import ./overlays;         # nixpkgs overlays
 
-  chatterino = pkgs.callPackage ./pkgs/chat/chatterino {};
-  # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
-  # ...
+      chatterino = callPackage ./pkgs/chat/chatterino {};
+    };
 }
+
