21712c2b165636a04de94965e4463eb8a43f08b1
Richard Annand <richard@ohheyrj.co.uk>
2025-05-22 09:43:11 +0100

    Revert "fix: changed how packages are called"
    This reverts commit 643eb7b57f45a3551660fd2bd7158008f688ad7c.

--- default.nix
+++ default.nix
@@ -4,12 +4,19 @@
 let
   inherit (pkgs) lib stdenv;
 
+  sharedOverrides = {
+    versionCheckHook = pkgs.versionCheckHook;
+    _7zz = pkgs._7zz or pkgs.sevenZip;
+    writeShellScript = pkgs.writeShellScriptBin or pkgs.writeShellScript;
+    xcbuild = pkgs.xcbuild;
+  };
+
   maybeEnable = drv:
     if lib.elem stdenv.hostPlatform.system (drv.meta.platforms or []) then drv else null;
 
-  openaudible = maybeEnable (pkgs.callPackage ./pkgs/media/openaudible {});
+  openaudible = maybeEnable (pkgs.callPackage ./pkgs/media/openaudible sharedOverrides);
   chatterino  = maybeEnable (pkgs.callPackage ./pkgs/chat/chatterino {});
-  kobo-desktop = maybeEnable (pkgs.callPackage ./pkgs/media/kobo-desktop {});
+  kobo-desktop = maybeEnable (pkgs.callPackage ./pkgs/media/kobo-desktop sharedOverrides);
 
 in {
   lib = import ./lib { inherit pkgs; };
