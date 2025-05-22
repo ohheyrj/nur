fe4fcf474a4d2d901974826380c0ef1c338fe221
Richard Annand <richard@ohheyrj.co.uk>
2025-05-22 09:26:14 +0100

    fix: changed how packages are called

--- default.nix
+++ default.nix
@@ -1,42 +1,27 @@
 
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import <nixpkgs> {} }:
 
 let
   inherit (pkgs) lib stdenv;
 
-  packageOverrides = {
+  sharedOverrides = {
     versionCheckHook = pkgs.versionCheckHook;
-    _7zz = pkgs._7zz or pkgs.sevenZip; # fallback if using older nixpkgs
+    _7zz = pkgs._7zz or pkgs.sevenZip;
     writeShellScript = pkgs.writeShellScriptBin or pkgs.writeShellScript;
     xcbuild = pkgs.xcbuild;
   };
 
-  findDefaultNixFiles = path:
-    lib.flatten (lib.mapAttrsToList (name: type:
-      let fullPath = "${toString path}/${name}";
-      in if type == "directory" then findDefaultNixFiles fullPath
-         else if name == "default.nix" then [ fullPath ]
-         else []
-    ) (builtins.readDir path));
+  maybeEnable = drv:
+    if lib.elem stdenv.hostPlatform.system (drv.meta.platforms or []) then drv else null;
 
-  defaultNixFiles = findDefaultNixFiles ./pkgs;
-
-  deriveName = path:
-    builtins.baseNameOf (toString (builtins.dirOf path));
-
-  rawPackages = lib.genAttrs (map deriveName defaultNixFiles) (name:
-    let
-      file = lib.findFirst (p: deriveName p == name) null defaultNixFiles;
-      drv = pkgs.callPackage file packageOverrides;
-    in
-      if lib.elem stdenv.hostPlatform.system (drv.meta.platforms or []) then drv else null
-  );
-
-  filteredPackages = lib.filterAttrs (_: v: v != null) rawPackages;
+  openaudible = maybeEnable (pkgs.callPackage ./pkgs/openaudible sharedOverrides);
+  chatterino  = maybeEnable (pkgs.callPackage ./pkgs/chat/chatterino {});
 
 in {
   lib = import ./lib { inherit pkgs; };
   modules = import ./modules;
   overlays = import ./overlays;
-} // filteredPackages
+} // lib.filterAttrs (_: v: v != null) {
+  inherit openaudible chatterino;
+}
 
