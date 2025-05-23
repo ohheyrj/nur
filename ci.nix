9da879ca9a79ee19b6bcb29598530917e835b9d9
Richard Annand <richard@ohheyrj.co.uk>
2025-05-23 02:35:55 +0100

    chore: reverted to make things easier

--- ci.nix
+++ ci.nix
@@ -1,16 +1,24 @@
-# This file provides all the buildable packages and package outputs in your
-# package set. These are what gets built by CI, so if you correctly mark
-# packages as broken your CI will not try to build them and the cache
-# will not contain them.
+# This file provides all the buildable and cacheable packages and
+# package outputs in your package set. These are what gets built by CI,
+# so if you correctly mark packages as
+#
+# - broken (using `meta.broken`),
+# - unfree (using `meta.license.free`), and
+# - locally built (using `preferLocalBuild`)
+#
+# then your CI will be able to build and cache only those packages for
+# which this is possible.
 
 { pkgs ? import <nixpkgs> { } }:
 
 with builtins;
-
 let
   isReserved = n: n == "lib" || n == "overlays" || n == "modules";
   isDerivation = p: isAttrs p && p ? type && p.type == "derivation";
-  isBuildable = p: !(p.meta.broken or false) && p.meta.license != lib.licenses.unfree;
+  isBuildable = p: let
+    licenseFromMeta = p.meta.license or [];
+    licenseList = if builtins.isList licenseFromMeta then licenseFromMeta else [licenseFromMeta];
+  in !(p.meta.broken or false) && builtins.all (license: license.free or true) licenseList;
   isCacheable = p: !(p.preferLocalBuild or false);
   shouldRecurseForDerivations = p: isAttrs p && p.recurseForDerivations or false;
 
@@ -22,10 +30,10 @@ let
     let
       f = p:
         if shouldRecurseForDerivations p then flattenPkgs p
-        else if isDerivation p then [p]
-        else [];
+        else if isDerivation p then [ p ]
+        else [ ];
     in
-      concatMap f (attrValues s);
+    concatMap f (attrValues s);
 
   outputsOf = p: map (o: p.${o}) p.outputs;
 
@@ -33,13 +41,12 @@ let
 
   nurPkgs =
     flattenPkgs
-    (listToAttrs
-    (map (n: nameValuePair n nurAttrs.${n})
-    (filter (n: !isReserved n)
-    (attrNames nurAttrs))));
+      (listToAttrs
+        (map (n: nameValuePair n nurAttrs.${n})
+          (filter (n: !isReserved n)
+            (attrNames nurAttrs))));
 
 in
-
 rec {
   buildPkgs = filter isBuildable nurPkgs;
   cachePkgs = filter isCacheable buildPkgs;
